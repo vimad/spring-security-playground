e2dab31160acc91dd5cf2b7f596f9803dc3a5a64
vinod <vinod.madubashana@madmobile.com>
2025-02-02 08:30:51 +0530

    init project with starter web and a simple endpoint

---
+++ src/main/java/com/example/springsecurity/config/SecurityFilterChainConfig.java
@@ -0,0 +1,14 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityFilterChainConfig {
+
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //     return http
+    //         .build();
+    // }
+}
