e21b58d8ba2a9da611120224e24dbfa50b0ab3b6
vinod <vinod.madubashana@madmobile.com>
2025-02-02 12:20:49 +0530

    configure DelegatingPasswordEncoder

--- src/main/java/com/example/springsecurity/config/SecurityUserConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityUserConfig.java
@@ -6,24 +6,49 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class SecurityUserConfig {
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user = User.withUsername("vinod")
-            .password(passwordEncoder.encode("123"))
+        UserDetails user1 = User.withUsername("vinod")
+            .password("{bcrypt}" + new BCryptPasswordEncoder().encode("123"))
             .build();
-        return new InMemoryUserDetailsManager(List.of(user));
+        UserDetails user2 = User.withUsername("test")
+            .password("{noop}456")
+            .build();
+        return new InMemoryUserDetailsManager(List.of(user1, user2));
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
     }
+
+    // @Bean
+    // public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+    //     UserDetails user = User.withUsername("vinod")
+    //         .password(passwordEncoder.encode("123"))
+    //         .build();
+    //     return new InMemoryUserDetailsManager(List.of(user));
+    // }
+    //
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
 }
