78ed9ab4d3973580f55240e4b04412541f3a9b1b
vinod <vinod.madubashana@madmobile.com>
2025-02-05 09:40:46 +0530

    listen to authentication events

--- src/main/java/com/example/springsecurity/config/AppConfig.java
+++ src/main/java/com/example/springsecurity/config/AppConfig.java
@@ -1,17 +1,28 @@
 package com.example.springsecurity.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableAsync
+@Slf4j
 public class AppConfig {
     @Bean
     public InitializingBean initializingBean() {
         return () -> SecurityContextHolder
             .setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
+
+    @Bean
+    public ApplicationListener<AuthenticationSuccessEvent> authenticationSuccessListener() {
+        return event -> {
+            log.info("Authentication success with user {}", event.getAuthentication().getName());
+        };
     }
 }
